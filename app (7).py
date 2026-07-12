8cbe76c4919d9b25b6415ae4e66d6988765cc6e0
Estobam989 <williamsoluwatosin88@gmail.com>
2026-07-12 01:25:41 +0100

    Add ChatHuggingFace import to app

--- app (7).py
+++ app (7).py
@@ -5,6 +5,7 @@ import streamlit as st
 import tempfile
 import shutil 
 
+from langchain_huggingface import ChatHuggingFace
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
