75fcaa062b02b0189edaa8362599e5e7c6e15dda
Estobam989 <williamsoluwatosin88@gmail.com>
2026-07-12 01:21:18 +0100

    Remove unused import for ChatHuggingFace

--- app (7).py
+++ app (7).py
@@ -5,7 +5,6 @@ import streamlit as st
 import tempfile
 import shutil 
 
-from langchain_huggingface import ChatHuggingFace 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
